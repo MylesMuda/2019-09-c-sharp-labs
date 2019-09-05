ba475e3df3465454801e9189bebf56e15aa04b39
Myles <Muda>
2019-09-05 12:20:20 +0100

    Added lab 16 - abstract classes

---
+++ visStudio/lab_09/lab_16_abstract_class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab_16_abstract_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var h = new Holiday();
+        }
+
+        abstract public class Holiday
+        {
+            public abstract void Travel();
+
+            public void Places() { Console.WriteLine("Visit Vienna, Salzburg"); }
+
+            public void Activites() { Console.WriteLine("Skiing, Walking, Fishing"); }
+        }
+
+        public class HolidayWithTravel : Holiday
+        {
+            public void Travel() { Console.WriteLine("By train, eurostar, hire a car"); }
+        }
+    }
+}
