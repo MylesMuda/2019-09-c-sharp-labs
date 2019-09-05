b76441348f344055ab7b6273339f03facf599d5c
Myles Muda <MMuda@spartaglobal.com>
2019-09-05 10:41:55 +0100

    added lab 15

---
+++ visStudio/lab_09/lab_15_stopwatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+namespace lab_15_stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Stopwatch();
+            s.Start();
+
+            var counter = 0;
+            for (int i = 0; i < 100_000_000; i++)
+            {
+                counter++;
+            }
+
+            var b = 0b_1010_1010_1010;
+            var c = 0x_ff_ee_dd_cc_bb;
+
+            s.Stop();
+            Console.WriteLine(s.Elapsed);
+            Console.WriteLine(s.ElapsedMilliseconds);
+            Console.WriteLine(s.ElapsedTicks);
+
+        }
+    }
+}
