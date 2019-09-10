8415e41dbbf94e0d1c7f85fddbe3faf05cae27d4
Myles <Muda>
2019-09-10 16:56:47 +0100

    Added Northwind server lab

--- visStudio/lab_09/lab_24_Northwind/Program.cs
+++ visStudio/lab_09/lab_24_Northwind/Program.cs
@@ -8,8 +8,26 @@ namespace lab_24_Northwind
 {
     class Program
     {
+        static List<Product> products;
+        static List<Category> categories;
         static void Main(string[] args)
         {
+            using(var db = new NorthwindEntities())
+            {
+                products = db.Products.ToList();
+                categories = db.Categories.ToList();
+            }
+
+            products.ForEach(p =>
+            {
+                Console.WriteLine($"ID: {p.ProductID,-10} Product: {p.ProductName,-35} Price: {p.UnitPrice,-10}");
+            });
+            Console.WriteLine("\n");
+            categories.ForEach(c =>
+            {
+                Console.WriteLine($"ID: {c.CategoryID, -10} Name: {c.CategoryName,-38} Description: {c.Description, -10}");
+            });
+            Console.ReadLine();
         }
     }
 }
